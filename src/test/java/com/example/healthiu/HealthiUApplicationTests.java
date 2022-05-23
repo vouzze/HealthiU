0dae6582f14de2001242602f8e2fb446437cb619
vouzze <voslavuhristu@gmail.com>
2022-05-23 20:36:36 +0300

    Initial commit

---
+++ src/test/java/com/example/healthiu/HealthiUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthiUApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
