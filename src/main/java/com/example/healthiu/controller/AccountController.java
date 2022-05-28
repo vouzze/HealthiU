93c230f72879c34b78d884e67e1a862cad1cff64
vouzze <voslavuhristu@gmail.com>
2022-05-28 05:17:04 +0300

    added profile edit

--- src/main/java/com/example/healthiu/controller/AccountController.java
+++ src/main/java/com/example/healthiu/controller/AccountController.java
@@ -1,16 +1,21 @@
 package com.example.healthiu.controller;
 
 import com.example.healthiu.entity.User;
+import com.example.healthiu.entity.UserData;
 import com.example.healthiu.repository.UserRepository;
 import com.example.healthiu.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.Objects;
 
 @Controller
 public class AccountController {
@@ -23,12 +28,32 @@ public class AccountController {
         return "hello";
     }
 
+    @PostMapping("/hello/edit")
+    public String editProfile(@Valid UserData userData, BindingResult result, HttpServletRequest req, Model model) {
+        if (result.hasErrors()) {
+            System.out.println("Invalid user");
+            return "hello";
+        }
+        if (userService.checkIfEmailExist(userData.getEmail()) &&
+                !Objects.equals(userService.findUserByEmail(userData.getEmail()).getLogin(), userData.getLogin())) {
+            System.out.println("email exists");
+            model.addAttribute("email_exists", "Email already exists");
+            return "redirect:hello";
+        }
+        userService.updateUserInfo(userData);
+        return "hello";
+    }
+
     private void addAttributes(Model model, User user) {
-        model.addAttribute("login", user.getLogin());
-        model.addAttribute("name",user.getName() );
-        model.addAttribute("email", user.getEmail());
-        model.addAttribute("password", user.getPassword());
-        model.addAttribute("date_of_birth", user.getDateOfBirth());
+        UserData userData = new UserData();
+        BeanUtils.copyProperties(user, userData);
+        userData.setConfirmPassword(userData.getPassword());
+        model.addAttribute("userData", userData);
+        model.addAttribute("login", userData.getLogin());
+        model.addAttribute("name", userData.getName());
+        model.addAttribute("email", userData.getEmail());
+        model.addAttribute("password", userData.getPassword());
+        model.addAttribute("dateOfBirth", userData.getDateOfBirth());
         model.addAttribute("role", user.getRole());
     }
 
